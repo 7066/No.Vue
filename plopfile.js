a2171b36451e5d7e99edf095677c6eb2472df5bb
7066 <7066@GAMING>
2024-01-24 10:53:29 +0800

    feat: ✨  更改 plop 默认选项为 commit

--- plopfile.js
+++ plopfile.js
@@ -2,7 +2,7 @@ import _module from "./build/module.js";
 import _component from "./build/component.js";
 import _commit from "./build/commit.js";
 export default (plop) => {
+  plop.setGenerator("commit", _commit);
   plop.setGenerator("module", _module);
   plop.setGenerator("component", _component);
-  plop.setGenerator("commit", _commit);
 };
