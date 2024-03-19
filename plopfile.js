0e615189a7b9105843344627d304ab8b49342505
7066 <7066@GAMINg>
2024-03-19 16:29:02 +0800

    feat: readme

--- plopfile.js
+++
@@ -1,8 +0,0 @@
-import _module from "./.plop/module.js";
-import _component from "./.plop/component.js";
-import _commit from "./.plop/commit.js";
-export default (plop) => {
-  plop.setGenerator("commit", _commit);
-  plop.setGenerator("module", _module);
-  plop.setGenerator("component", _component);
-};
--- src/modules/example2/views/blue.vue
+++
@@ -1,1 +0,0 @@
-<template>BLUE</template>
